9be5a29406701d167441463209cf6e3d890e377c
nham <hamann.nick@gmail.com>
2014-07-06 15:21:11 -0400

    Change crate type to only be 'lib'. Maybe I only need that.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-// I don't understand why both are needed, but rust-http has both.
-#![crate_type = "dylib"]
-#![crate_type = "rlib"]
+#![crate_type = "lib"]
 
 pub mod common;
 pub mod request;
 pub mod response;
+pub mod status;
